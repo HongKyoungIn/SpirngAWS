00d466d9f7a50dcad05a0854279edbe880f0db82
HGI <ghdruddls999@naver.com>
2021-11-04 02:20:17 +0900

    2021-11-03(2) Springboot_AWS Study

--- src/main/java/com/jojoldu/book/springboot/web/PostApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostApiController.java
@@ -2,12 +2,12 @@ package com.jojoldu.book.springboot.web;
 
 
 import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor //compile 시 final 필드변수들에게 매개변수로 생성자 생성
 @RestController
@@ -20,4 +20,13 @@ public class PostApiController {
         return postsService.save(requestDto);
     }
 
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findByID (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 }
