dd3436120105b7026ab78688686622c3decee927
HGI <ghdruddls999@naver.com>
2021-11-03 21:14:42 +0900

    2021-11-03 Springboot_AWS Study

---
+++ src/main/java/com/jojoldu/book/springboot/web/PostApiController.java
@@ -0,0 +1,23 @@
+package com.jojoldu.book.springboot.web;
+
+
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor //compile 시 final 필드변수들에게 매개변수로 생성자 생성
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+}
